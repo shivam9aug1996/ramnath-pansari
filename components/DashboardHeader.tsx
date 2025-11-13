c0d94a2c1ce134f03baade03ff8195f9b854a34f
shivam garg <shivamgarg@shivams-MacBook-Air.local>
2025-11-13 08:13:22 +0530

    temp channge

--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -32,7 +32,7 @@ const DashboardHeader = ({ userName, profileImage, onProfilePress,isGuestUser })
 /> 
 ) : (
   <Text style={styles.greetingText}>
-    {`Hey ${userName} garg `}
+    {`Hey1 ${userName} garg `}
   </Text>
 )}
 
